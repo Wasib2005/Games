88be5fba1d5a1051af292a8b21b538e319adedc1
Wasib2005 <wasibulislam8984@gmail.com>
2024-07-12 16:15:29 +0600

    Little update

--- rec/Third-game/rec/setupNumber.js
+++ rec/Third-game/rec/setupNumber.js
@@ -1,83 +1,39 @@
-const levels =[
-    `
-1_34_67_9
-1_34_6__9
-12_4_67_9
-1234_6789
-1_345__89
-1___56789
-1______89
-1_34_6_89
-12_456_89
-`
-    ,
-    `
-2_3456_89
-_________
-12_456789
-1234_6789
-12345__89
-1___56789
-1______89
-123456_89
-12_456_89
-`
-    ,
-    `
-323456789
-1_3456789
-_________
-1234_6789
-12345__89
-1___56789
-1______89
-123456_89
-12_456_89
-`
-    ,
-    `
-423456789
-1_3456789
-12_456789
-_________
-_________
-1___56789
-1______89
-123456_89
-12_456_89
-`
-    ,
+const levels = ["84___5___3_198_2___2961738__9_37__45______89_185_967_3_58_6___2_1___95_8_7___2461", "148362975_5_4_9___29______3_1___8_948__291_3___974682___562341___318__57461_7__82", "_168_3_59_______17_7_2_16___69_2__488_7_695232_14__79_19_68_4_____5___616_53__97_", "______189_____3467_7_9863__9_38___1_7_54_____28__7_5_6459_31_78_____89313_8697___"
 ]
 
 
-let level=(levels[Math.floor(Math.random() * (levels.length - 0)) + 0]).split('')
+let level = (levels[Math.floor(Math.random() * (levels.length - 0)) + 0]).split('')
 
 
-let tempLevel = []
 
 
-level.forEach(element => {
-    if (element !== ' ' && element !== '\n') {
-        tempLevel.push(element)
-    }
-});
+function levelToTempLevel(level) {
+    let tempLevel = []
+    level.forEach(element => {
+        if (element !== ' ' && element !== '\n') {
+            tempLevel.push(element)
+        }
+    });
+    return tempLevel
+}
 
 
-function newGame(value){
+
+function newGame(value) {
     n = 1
     value.forEach(element => {
-        let docElement=document.getElementById(`place${n}`)
-        if (element in [1,2,3,4,5,6,7,8,9]){
-            console.log(element)
-            docElement.setAttribute("placeholder",`${element}`)
-            docElement.setAttribute("readonly",true)
+        let docElement = document.getElementById(`place${n}`)
+        if (element in [1, 2, 3, 4, 5, 6, 7, 8, 9]) {
+            docElement.setAttribute("placeholder", `${element}`)
+            docElement.setAttribute("readonly", true)
             docElement.classList.add("bg-slate-500")
-
         }
-
-
         n++
     });
-    
+
 }
-newGame(tempLevel)
+newGame(levelToTempLevel(level))
+function reset() {
+
+
+}
